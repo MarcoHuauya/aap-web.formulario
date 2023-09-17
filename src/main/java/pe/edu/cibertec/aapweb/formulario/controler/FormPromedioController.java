dc595139b3b8270ad089a9e1ed187dc627e630ca
MarcoHuauya <marcohuauyalopez@gmail.com>
2023-09-17 15:57:28 -0500

    diseño de formulario

---
+++ src/main/java/pe/edu/cibertec/aapweb/formulario/controler/FormPromedioController.java
@@ -0,0 +1,4 @@
+package pe.edu.cibertec.aapweb.formulario.controler;
+
+public class FormPromedioController {
+}
---
+++ src/main/java/pe/edu/cibertec/aapweb/formulario/model/PromedioModel.java
@@ -0,0 +1,4 @@
+package pe.edu.cibertec.aapweb.formulario.model;
+
+public class PromedioModel {
+}
